88a9a9ef3410ebebdd04b625d60eb50ed4396be0
John Larson <jl@buildmidwestern.com>
2024-01-10 20:11:23 -0600

    initial static page commit

---
+++ src/main/kotlin/com/example/plugins/Templating.kt
@@ -0,0 +1,15 @@
+package com.example.plugins
+
+import freemarker.cache.*
+import io.ktor.server.application.*
+import io.ktor.server.freemarker.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureTemplating() {
+    install(FreeMarker) {
+        templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
+    }
+    routing {
+    }
+}
